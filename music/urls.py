ce33d1e01c9d127d0787fbda414268bd83cdc8fc
GeovanaRamos <geovannna.1998@gmail.com>
2018-11-22 15:07:48 -0200

    Formatting navbar for music and dashboard apps

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'music'
 urlpatterns = [
     path('', views.MyMusicView.as_view(), name='my_music'),
 ]
--- music/views.py
+++ music/views.py
@@ -2,4 +2,4 @@ from django.views.generic.base import TemplateView
 
 
 class MyMusicView(TemplateView):
-    template_name = "dashboard/my_music.html"
+    template_name = "music/my_music.html"
