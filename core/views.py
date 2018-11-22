6f1cbde0029b65a5b28b9802d01e8ddb7cd73de5
Joberth Rogers <joberth.rogers18@gmail.com>
2018-11-22 18:35:10 -0200

    creating page sign in

--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.base import TemplateView
-
-
-class SignInView(TemplateView):
-    template_name = "core/sign_in.html"
-
-
-class SignUpView(TemplateView):
-    template_name = "core/sign_up.html"
--- ihc/urls.py
+++ ihc/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('account/', include('account.urls')),
+    path('#', include('core.urls')),
+    path('', include('account.urls')),
 ]
