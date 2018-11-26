1194a49c27651485be19296404c5b34dd6c27a5a
GeovanaRamos <geovannna.1998@gmail.com>
2018-11-26 20:23:05 -0200

    Changing nav bar play button state when album is played

--- core/static/core/js/player.js
+++ core/static/core/js/player.js
@@ -12,6 +12,7 @@ $('#play-icon').click(function() {
 $("a[id*='album-play']").each(function (i, el) {
     $(this).click(function() {
         var audio = document.getElementById("player");
+        $('#play-icon').find('i').toggleClass('fa-play fa-pause')
         $(this).find('i').toggleClass('fa-play fa-pause')
         if($(this).find('i').hasClass('fa-pause')){
             audio.play();
