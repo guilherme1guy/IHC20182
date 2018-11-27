6bd395e21cbad674dbafe306cb91693e1006ce18
GeovanaRamos <geovannna.1998@gmail.com>
2018-11-27 14:53:41 -0200

    Making play button from playlist cover functional

--- music/static/music/js/album_play.js
+++ music/static/music/js/album_play.js
@@ -10,3 +10,16 @@ $("a[id*='album-play']").each(function (i, el) {
         }
     });
 });
+
+$("a[id*='playlist-play']").each(function (i, el) {
+    $(this).click(function() {
+        var audio = document.getElementById("player");
+        $('#play-icon').find('i').toggleClass('fa-play fa-pause')
+        $(this).find('i').toggleClass('fa-play fa-pause')
+        if($(this).find('i').hasClass('fa-pause')){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+});
