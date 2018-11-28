aa9e28eb20c1ca72b2d5a96fbba809fde66dc139
GeovanaRamos <geovannna.1998@gmail.com>
2018-11-28 10:47:11 -0200

    Styling settings and adding link to logout

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'account'
 urlpatterns = [
     path('', views.SignInView.as_view(), name='sign_in'),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
