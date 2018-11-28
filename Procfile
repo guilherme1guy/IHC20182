4dfdb0cd9e3f06474d7bf89f80d540c7bed81860
Guilherme Guy <guilhermeguy349@gmail.com>
2018-11-28 12:13:27 +0000

    Adding heroku deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn ihc.wsgi:application
+
--- ihc/settings.py
+++ ihc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,6 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+# Heroku deploy
+django_heroku.settings(locals())
